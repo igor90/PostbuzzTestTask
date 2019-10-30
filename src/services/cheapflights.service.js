808ee854a1756b3d74da7c95ab97edf15a62fe52
Volodymyr Prytula <vprytula@VT7.local>
2019-10-30 11:41:53 +0200

    Postbuzz test task

--- src/services/cheapflights.service.js
+++ src/services/cheapflights.service.js
@@ -2,7 +2,6 @@ class CheapFlightsService {
   constructor($http) {
     this.$http = $http;
   }
-  
   getAllTrips(stateParams) {
     const url = `https://murmuring-ocean-10826.herokuapp.com/en/api/2/flights/from/${stateParams.origin}/to/${stateParams.dest}/${stateParams.originDate}/${stateParams.departureDate}/250/unique/?limit=15&offset-0`;
     return this.$http.get(url);
